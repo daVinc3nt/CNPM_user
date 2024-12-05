4ceda62bbdb7b4b3c74c1977f48c70bf26daa42c
QuangTran013 <113117168+daVinc3nt@users.noreply.github.com>
2024-12-06 01:16:41 +0700

    ok

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,22 +10,22 @@ const intlMiddleware = createMiddleware({
   defaultLocale: 'en'
 });
  
-const authMiddleware = withAuth(
-  // Note that this callback is only invoked if
-  // the `authorized` callback has returned `true`
-  // and not for pages listed in `pages`.
-  function onSuccess(req) {
-    return intlMiddleware(req);
-  },
-  {
-    callbacks: {
-      authorized: ({token}) => true
-    },
-    pages: {
-      signIn: '/login'
-    }
-  }
-);
+// const authMiddleware = withAuth(
+//   // Note that this callback is only invoked if
+//   // the `authorized` callback has returned `true`
+//   // and not for pages listed in `pages`.
+//   function onSuccess(req) {
+//     return intlMiddleware(req);
+//   },
+//   {
+//     callbacks: {
+//       authorized: ({token}) => true
+//     },
+//     pages: {
+//       signIn: '/login'
+//     }
+//   }
+// );
  
 export default function middleware(req: NextRequest) {
   const publicPathnameRegex = RegExp(
@@ -39,7 +39,8 @@ export default function middleware(req: NextRequest) {
   if (isPublicPage) {
     return intlMiddleware(req);
   } else {
-    return (authMiddleware as any)(req);
+    return intlMiddleware(req);
+    // return (authMiddleware as any)(req);
   }
 }
  
